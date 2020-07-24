fa4a7c786dedae005ea0fe3cff3af28601ee2557
Zach McWhirter <zmac7811@gmail.com>
2020-07-24 13:50:55 -0500

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TopBar from "./TopBar";
+import {Router, Route, Link} from "react-router-dom";
+import {createBrowserHistory as createHistory} from "history";
+import HomePage from "./HomePage";
+
+const history = createHistory();
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <TopBar />
+        <Route
+          path="/"
+          exact
+          component={HomePage}
+        />
+      </Router>
     </div>
   );
 }
